3b3d666932848f9ff29b5ee7f231ae7e424579d4
Rodrigo Sanabria <r.sanabriadc@gmail.com>
2014-05-01 18:07:28 -0500

    Recorredor de arbol listo

--- Archivos y Directorios/miFinder/miFinder.c
+++ Archivos y Directorios/miFinder/miFinder.c
@@ -5,26 +5,86 @@
 #include<sys/types.h>
 #include<ftw.h>
 #include<unistd.h>
+#include<fcntl.h>
+#include<time.h>
+#include <pwd.h>
+#include <grp.h>
+
+char *query;
+DIR *dir;
+struct stat buffer;
+struct dirent *mi_dir;
+
+void archivo(struct stat buffer,const char * path){
+
+char tiempo[100],bufferTiempo[100];
+
+struct passwd *pwd = getpwuid(buffer.st_uid);
+struct group *gr = getgrgid(buffer.st_gid);
+strftime (bufferTiempo, 100, "%d.%m.%Y %H:%M ", localtime(&buffer.st_mtime)); 
+ 
+    printf( (S_ISDIR(buffer.st_mode)) ? "d" : "-");
+    printf( (buffer.st_mode & S_IRUSR) ? "r" : "-");
+    printf( (buffer.st_mode & S_IWUSR) ? "w" : "-");
+    printf( (buffer.st_mode & S_IXUSR) ? "x" : "-");
+    printf( (buffer.st_mode & S_IRGRP) ? "r" : "-");
+    printf( (buffer.st_mode & S_IWGRP) ? "w" : "-");
+    printf( (buffer.st_mode & S_IXGRP) ? "x" : "-");
+    printf( (buffer.st_mode & S_IROTH) ? "r" : "-");
+    printf( (buffer.st_mode & S_IWOTH) ? "w" : "-");
+    printf( (buffer.st_mode & S_IXOTH) ? "x " : "- ");
+    
+    printf("%d ",buffer.st_nlink);
+    
+    printf("%s ", pwd->pw_name);
+    printf("%s ", gr->gr_name);
+        
+     printf("%d ",buffer.st_size);
+ 
+    printf("%s",bufferTiempo);
+   printf("%s\n",path);
+}
+void directorio(const char *path){
+
+
+dir = opendir(path);
+    
+while(( mi_dir = readdir(dir)) != NULL){
+   if(strcmp(mi_dir->d_name,"." ) && strcmp(mi_dir->d_name,".."))  {
+        if(!strcmp(query,"*"))
+            query = mi_dir->d_name;
+        if(!strcmp(mi_dir->d_name,query)){
+                stat(mi_dir->d_name,&buffer);
+                archivo(buffer,mi_dir->d_name);
+        }
+}
+}
+    
+closedir(dir);
+}
+
 
 int main (int argc, char *argv[]){
 
-int dir (const char *, const struct stat *, int);
-
-if(argc == 1)
-  ftw(".",dir,10);
+int callback (const char *, const struct stat *, int);
+if (argv[2])
+    query = argv[2];
 else
- ftw(argv[1],dir,10);
+    query = "*";
+if(argc == 1)
+  ftw(".",callback,100);
+else
+ ftw(argv[1],callback,1);
 return 0;
 
 }
 
-int dir(const char *nombre, const struct stat *status, int type){
+int callback(const char *nombre, const struct stat *status, int type){
 
 if(type == FTW_NS)
    return 0;
 if (type == FTW_D)
- //printf((status->st_mode & S_IRUSR) ? "r\n" :"-\n");
- printf("%s\n",nombre);
+    directorio(nombre);
 
 return 0;
 }
